5a716eb0fc6949f9a34db6f03d4349c5155f22c0
hongda5 <hongda5@staff.sina.com.cn>
2020-04-10 19:57:15 +0800

    滑动返回

--- app/src/main/java/com/example/syncrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/syncrecyclerview/MainActivity.java
@@ -1,23 +1,40 @@
 package com.example.syncrecyclerview;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.viewpager.widget.ViewPager;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+public class MainActivity extends SwipeBackActivity {
 
     private ViewPager viewPager;
     private TestAdapter adapter;
+
+
+    private SwipeBackLayout mSwipeBackLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.viewpager);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         adapter = new TestAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(2);
+
+        // 可以调用该方法，设置是否允许滑动退出
+        setSwipeBackEnable(true);
+
+        mSwipeBackLayout = getSwipeBackLayout();
+
+        // 设置滑动方向，可设置EDGE_LEFT, EDGE_RIGHT, EDGE_ALL, EDGE_BOTTOM
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+
+        // 滑动退出的效果只能从边界滑动才有效果，如果要扩大touch的范围，可以调用这个方法
+        mSwipeBackLayout.setEdgeSize(200);
 
     }
 
